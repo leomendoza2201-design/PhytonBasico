408f4372aeb5fe16babc849d4f90a416bdb13aa0
leomendoza2201-design <leomendoza2201@gmail.com>
2026-02-13 15:24:08 -0800

    HICE OTRO CODIGO DE LISTA DE DICCIONARIO

---
+++ sesion_02.py
@@ -0,0 +1,18 @@
+# Tuplas
+mi_tupla = (2, 4)
+print("Mi tupla: ")
+
+# Lista
+mi_lista = [1, 3.1416, "Leo", mi_tupla]
+print("el primer elemento de mi lista: ",mi_lista[0])
+print("el cuarto elemento de mi lista: ",mi_lista[3])
+print("el tercer elemento de mi lista: ",mi_lista[2])
+
+# Diccionarios
+mi_diccionario = {"mi lista" : mi_lista,
+                  "nombre": "Leo",
+                  "pi": 3.1416,
+                  "Tel":"664-8566534"}
+print("llave para acceder a mi diccionario mi_lista",mi_diccionario["mi lista"])
+print("llava para acceder a mi diccionario pi",mi_diccionario["pi"])
+print("llave para acceder a mi diccionario tel",mi_diccionario["Tel"])
